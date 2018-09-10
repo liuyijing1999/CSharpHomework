2a0c65564cab2c585c0a86d027f58d172951553a
liuyijing1999 <2554246174@qq.com>
2018-09-10 23:45:21 +0800

    提交C#作业
    计算两个数的乘积

---
+++ homework1/program1/program1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "";
+            Console.Write("a = ");
+            a = Console.ReadLine();
+            int m = 0;
+            m = Int32.Parse(a);
+            string b = "";
+            Console.Write("b = ");
+            b = Console.ReadLine();
+            int n = 0;
+            n = Int32.Parse(b);
+            Console.WriteLine("a * b = " + (m * n));
+        }
+    }
+}
